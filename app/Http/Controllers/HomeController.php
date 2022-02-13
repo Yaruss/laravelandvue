e44abc0971b83d7244669e1cf344429cd0ed5906
Yarus <yaruss@gmail.com>
2022-02-13 17:46:01 +0300

    Создание главной страници и контролера для нее.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $articles = Article::orderBy('created_at', 'desc')->limit(6)->get();
+        return view('app.home', compact('articles'));
+    }
+}
---
+++ resources/views/app/home.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row mt-5">
+        @foreach($articles as $article)
+            <div class="col-6 pb-3">
+                <div class="card">
+                    <img src="{{$article->img}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$article->title}}</h5>
+                        <p class="card-text"></p>
+                        <p></p>
+                        <a href="" class="btn btn-primary">Подробнее</a>
+                        <div class="mt-3">
+                            <span class="badge bg-primary">{{$article->state->likes}} <i class="far fa-thumbs-up"></i></span>
+                            <span class="badge bg-danger">{{$article->state->views}} <i class="far fa-eye"></i></span>
+                        </div>
+                        <div class="mt-4">
+                            Теги:
+                            @foreach ($article->tags as $tag)
+                                <a href="" class="badge bg-danger">{{$tag->label}}</a>
+                            @endforeach
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
